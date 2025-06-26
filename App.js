4f26708a5cc8a1b2fb2c00f46551aa7118ec241c
Christopher Miiro <christopher.miiro@gmail.com>
2025-06-26 12:08:09 +0300

    Navigation theme

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import navtheme from "./app/navigation/navtheme";
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={navtheme}>
       <AuthNavigator />
     </NavigationContainer>
   );
