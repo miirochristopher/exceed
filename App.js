ac1c680b261ba816c4b51116b783ea62342fef8d
Christopher Miiro <christopher.miiro@gmail.com>
2025-06-26 19:27:34 +0300

    Remove unused imports

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import AuthNavigator from "./app/navigation/AuthNavigator";
 import navtheme from "./app/navigation/navtheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 
