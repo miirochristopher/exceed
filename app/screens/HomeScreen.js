1fdb0da52f132513e59ae2227896bb0e8b9fa2f8
Christopher Miiro <christopher.miiro@gmail.com>
2025-08-28 22:14:12 +0300

    (feat) meetings form

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+
+import Button from "../components/Button";
+import colors from "../config/colors";
+
+function HomeScreen({ navigation }) {
+  return (
+    <SafeAreaView style={styles.background}>
+      <View style={styles.buttonsContainer}>
+        <Text style={styles.events}>Club Events/Meetings</Text>
+        <Button
+          color="secondary"
+          title="Physical meetings"
+          onPress={() => navigation.push("Meeting")}
+        />
+        <Button
+          title="Social meetings"
+          color="secondary"
+          onPress={() => navigation.push("Events")}
+        />
+        <Button
+          title="Speech Contests"
+          color="secondary"
+          onPress={() => navigation.push("Events")}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+    marginBottom: "25%",
+  },
+  events: {
+    alignContent: "center",
+    color: colors.primary,
+    display: "flex",
+    fontWeight: 600,
+    fontSize: 28,
+    justifyContent: "center",
+    marginLeft: 20,
+    marginBottom: 35,
+    marginTop: 35,
+  },
+});
+
+export default HomeScreen;
